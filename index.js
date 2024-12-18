1e57037d76b465ddad72061d19de51b92110959c
janvierjr <bernie@myheartsleeve.com>
2024-12-18 16:46:33 -0600

    add mega menu hamburger active toggle

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
   // DOM ready code
   console.log('DOM is ready')
+
+  /** Mega Menu + Hamburger - Open and Close - Active Class Toggle */
+  const hamburgerButton = document.querySelector('.btn-hamburger');
+  const offCanvasMegaMenu = document.querySelector('.offcanvas-mega-menu');
+  const page = document.querySelector('.page');
+
+  hamburgerButton.addEventListener('click', () => {
+    offCanvasMegaMenu.classList.toggle('active');
+    hamburgerButton.classList.toggle('active');
+    
+    // prevents page scroll with Mega Menu active / open
+    page.classList.toggle('overflow-hidden');
+  });
+
 });
