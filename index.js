6e3f37140fb41ebb84f7e3a33d2df65b671f10fe
janvierjr <bernie@myheartsleeve.com>
2024-12-18 22:04:51 -0600

    update active class toggle function with offcanvas-circle-expand

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ document.addEventListener('DOMContentLoaded', function () {
   /** Mega Menu + Hamburger - Open and Close - Active Class Toggle */
   const hamburgerButton = document.querySelector('.btn-hamburger');
   const offCanvasMegaMenu = document.querySelector('.offcanvas-mega-menu');
+  const offCanvasCircleExpand = document.querySelector('.offcanvas-circle-expand');
   const page = document.querySelector('.page');
 
   hamburgerButton.addEventListener('click', () => {
+    offCanvasCircleExpand.classList.toggle('active');
     offCanvasMegaMenu.classList.toggle('active');
     hamburgerButton.classList.toggle('active');
-    
+
     // prevents page scroll with Mega Menu active / open
     page.classList.toggle('overflow-hidden');
   });
