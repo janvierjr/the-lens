6edbe1037633bccb4989a03b20f9e33cff95af22
janvierjr <bernie@myheartsleeve.com>
2024-12-20 16:23:55 -0600

    update handleHamburgerMenuClick for aria-expanded

--- index.js
+++ index.js
@@ -2,19 +2,26 @@ document.addEventListener('DOMContentLoaded', function () {
   // DOM ready code
   console.log('DOM is ready')
 
-  /** Mega Menu + Hamburger - Open and Close - Active Class Toggle */
   const hamburgerButton = document.querySelector('.btn-hamburger');
   const offCanvasMegaMenu = document.querySelector('.offcanvas-mega-menu');
   const offCanvasCircleExpand = document.querySelector('.offcanvas-circle-expand');
   const page = document.querySelector('.page');
 
-  hamburgerButton.addEventListener('click', () => {
+  /** Mega Menu + Hamburger - Open and Close - Active Class Toggle function */
+  const handleHamburgerButtonClick = (e) => {
+    e.preventDefault();
     offCanvasCircleExpand.classList.toggle('active');
     offCanvasMegaMenu.classList.toggle('active');
     hamburgerButton.classList.toggle('active');
 
+    // update Aria-expanded Attribute
+    offCanvasMegaMenu.getAttribute('aria-expanded') === 'true'
+      ? offCanvasMegaMenu.setAttribute('aria-expanded', 'false')
+      : offCanvasMegaMenu.setAttribute('aria-expanded', 'true');
+
     // prevents page scroll with Mega Menu active / open
     page.classList.toggle('overflow-hidden');
-  });
+  };
 
+  hamburgerButton.addEventListener('click', handleHamburgerButtonClick);
 });
